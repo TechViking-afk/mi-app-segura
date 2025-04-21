b900932dcfc6a49ca5734000300f11d63e08c197
David Rojo Villalba <118904055+ddrojovillalba@users.noreply.github.com>
2025-04-21 23:52:22 +0200

    test: exportar app y añadir endpoint de healthcheck para tests

--- app/src/index.js
+++ app/src/index.js
@@ -1,32 +1,58 @@
+// app/src/index.js
 const express = require('express');
-const app = express();
 const db = require('./persistence');
 const getItems = require('./routes/getItems');
 const addItem = require('./routes/addItem');
 const updateItem = require('./routes/updateItem');
 const deleteItem = require('./routes/deleteItem');
 
+const app = express();
+
+// Middlewares
 app.use(express.json());
 app.use(express.static(__dirname + '/static'));
 
+// Healthcheck para tests
+app.get('/', (_req, res) => {
+  res.sendStatus(200);
+});
+
+// Rutas de la API
 app.get('/items', getItems);
 app.post('/items', addItem);
 app.put('/items/:id', updateItem);
 app.delete('/items/:id', deleteItem);
 
-db.init().then(() => {
-    app.listen(3000, () => console.log('Listening on port 3000'));
-}).catch((err) => {
-    console.error(err);
-    process.exit(1);
-});
-
-const gracefulShutdown = () => {
+// Función para cerrar la DB y el servidor
+const gracefulShutdown = (server) => {
+  return () => {
     db.teardown()
-        .catch(() => {})
-        .then(() => process.exit());
+      .catch(() => {})
+      .then(() => {
+        if (server) server.close(() => process.exit(0));
+        else process.exit(0);
+      });
+  };
 };
 
-process.on('SIGINT', gracefulShutdown);
-process.on('SIGTERM', gracefulShutdown);
-process.on('SIGUSR2', gracefulShutdown); // Sent by nodemon
+// Si este fichero se ejecuta directamente con `node src/index.js`
+if (require.main === module) {
+  db.init()
+    .then(() => {
+      const server = app.listen(3000, () =>
+        console.log('Listening on port 3000')
+      );
+
+      // Captura de señales para shutdown limpio
+      process.on('SIGINT', gracefulShutdown(server));
+      process.on('SIGTERM', gracefulShutdown(server));
+      process.on('SIGUSR2', gracefulShutdown(server));
+    })
+    .catch((err) => {
+      console.error(err);
+      process.exit(1);
+    });
+}
+
+// Exportar la app para que los tests la importen sin arrancar el servidor
+module.exports = app;
